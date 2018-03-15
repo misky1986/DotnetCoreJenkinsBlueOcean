5f61c8c98de166e1eca54b93deaafd2fcde93697
Skydt <Michael.Skydt@beumer.com>
2018-03-15 09:29:37 +0100

    Initial commit

---
+++ BESD.TEST/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BESD.TEST
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestThatTrueIsTrue()
+        {
+            Assert.True(true, "True is not true");
+        }
+    }
+}
